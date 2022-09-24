8bc80ad82d8cd82ce825dc5b18d49c2e6faeef18
Marcus Vinícius Weil <vinicciusplay10@gmail.com>
2022-09-24 15:31:33 -0300

    Get Elements, show Elements

--- js/script.js
+++ js/script.js
@@ -5,17 +5,37 @@ const apiCountryURL = "https://countryflagsapi.com/png/";
 const cityInput = document.querySelector("#city-input");
 const searchBtn = document.querySelector("#search");
 
+const cityElement = document.querySelector("#city");
+const temperatureElement = document.querySelector("#temperature span");
+const conditionElement = document.querySelector("#description");
+const countryFlagElement = document.querySelector("#country-flag");
+const weatherIconElement = document.querySelector("#weather-icon");
+const umidityElement = document.querySelector("#umidity span");
+const windElement = document.querySelector("#wind span");
+
 // ** Functions
+
+const getWeatherData = async (city) => {
+    const apiWeatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}`
+}
+
+const showWeatherData = (city) => {
+
+    console.log(city)
+
+}
+
 
 
 // ** Events
 
 // Evento de clique no botão
 searchBtn.addEventListener("click", (e) => {
-
     // Evita erro ao fazer submit do formulário
     e.preventDefault();
 
-    console.log('clickEvent');
+    const city = cityInput.value;
+
+    showWeatherData(city);
 
 })
