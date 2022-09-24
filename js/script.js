8ae277594dd5eebbc08ee36b4b2d387d3d19cde8
Marcus Vinícius Weil <vinicciusplay10@gmail.com>
2022-09-24 15:15:17 -0300

    SearchBtn event Listener

--- js/script.js
+++ js/script.js
@@ -1,1 +1,21 @@
-// 2263d43a2f1bd15d4dd9f9f9c9060605
+// ? Open Weather API
+const apiKey = "2263d43a2f1bd15d4dd9f9f9c9060605";
+const apiCountryURL = "https://countryflagsapi.com/png/";
+
+const cityInput = document.querySelector("#city-input");
+const searchBtn = document.querySelector("#search");
+
+// ** Functions
+
+
+// ** Events
+
+// Evento de clique no botão
+searchBtn.addEventListener("click", (e) => {
+
+    // Evita erro ao fazer submit do formulário
+    e.preventDefault();
+
+    console.log('clickEvent');
+
+})
